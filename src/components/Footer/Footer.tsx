551303ce615aef7a1d35b42714ceeee16c8d090f
developer-junaid <qjunaid623@gmail.com>
2021-03-12 12:14:32 +0500

    Added links to footer, Deployed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import classes from "./footer.module.css";
+import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className={classes.wrapper}>
       <div className={classes.footer}>
-        <h3>Developer Junaid</h3>
+        <h3>
+          Developer <span>Junaid</span>
+        </h3>
+        <div className={classes.icons}>
+          <a href="https://github.com/developer-junaid">
+            <FaGithub />
+          </a>
+          <a href="https://www.facebook.com/JunaidQureshi6/">
+            <FaFacebook />
+          </a>
+          <a href="https://www.linkedin.com/in/developer-junaid/">
+            <FaLinkedin />
+          </a>
+        </div>
       </div>
     </footer>
   );
